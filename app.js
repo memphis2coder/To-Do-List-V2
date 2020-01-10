4106d0606f36235a371674d0084cf972057f9e28
Antonio <Memphis2Coder@gmail.com>
2020-01-09 21:22:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+function addItem() {
+  let item = document.getElementById("input").value;
+  let ul = document.getElementById("list");
+  let li = document.createElement("li");
+  li.appendChild(document.createTextNode(" " + item));
+  ul.appendChild(li);
+  document.getElementById("input").value = "";
+  li.onclick = removeItem;
+}
+
+document.body.onkeyup = function(e) {
+  if (e.keyCode === 13) {
+    addItem();
+  }
+};
+
+function removeItem(e) {
+  e.target.parentElement.removeChild(e.target);
+}
